6b647c5c553a7a38cd16ffdf3b6c9be516a0e20e
MTKknight48 <70550698+MTKknight48@users.noreply.github.com>
2020-12-13 21:47:37 -0500

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,45 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+        this.image1=loadImage("sprites/sling1.png");
+        this.image2=loadImage("sprites/sling2.png");
+        this.image3=loadImage("sprites/sling3.png");
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        image(this.image1,200,20);
+        image(this.image2,175,20);
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            
+            stroke(48,22,8);
+            if (pointA.x<200) {
+                strokeWeight(7);
+            line(pointA.x-25, pointA.y+10, pointB.x, pointB.y);
+            line(pointA.x-25, pointA.y+10, pointB.x+50, pointB.y);
+            image(this.image3,pointA.x-20,pointA.y-10,20,30);
+            }
+            else {
+                strokeWeight(4);
+                line(pointA.x-25, pointA.y+10, pointB.x, pointB.y);
+                line(pointA.x-25, pointA.y+10, pointB.x+50, pointB.y);
+                image(this.image3,pointA.x-20,pointA.y-10,20,30);
+            }
+            
+        }
+    }
+    
+}
